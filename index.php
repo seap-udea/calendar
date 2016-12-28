f28ba2ab112cb636edb26449af55bc555939260e
SEAP UDEA <seapudea@gmail.com>
2016-12-28 10:13:42 -0500

    Commit

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- HEADER -->
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<head>
+  <title>W3.CSS</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+  <!--
+      Font awesome: ttp://fontawesome.io/icons/
+  -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/calendar.css">
+  <link rel="stylesheet" href="js/flipclock/flipclock.css">
+  <script src="js/jquery.js"></script>
+  <script src="js/flipclock/flipclock.js"></script>	
+</head>
+
+<body class="w3-black">
+
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- ICON BAR (LARGE AND MEDIUM SCREENS) -->
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- Icon Bar (Sidenav - hidden on small screens) -->
+<nav class="w3-sidenav w3-center w3-small w3-hide-small">
+  <!-- Avatar image in top left corner -->
+  <img src="img/LogoSimbolo.png" width="100%">
+  <a class="w3-padding-large w3-black" href="#">
+    <i class="fa fa-home w3-xxlarge"></i>
+    <p>INICIO</p>
+  </a>
+  <a class="w3-padding-large w3-hover-black" href="#perihelio">
+    <!--<i class="fa fa-calendar-times-o fa-spin w3-xxlarge"></i>-->
+    <i class="fa fa-circle-o-notch fa-spin w3-xxlarge"></i>
+    <p>FIN DE AÑO</p>
+  </a>
+  <a class="w3-padding-large w3-hover-black" href="#contact">
+    <i class="fa fa-envelope w3-xxlarge"></i>
+    <p>CONTACT</p>
+  </a>
+</nav>
+
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- ICON BAR (SMALL SCREENS) -->
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- Navbar on small screens (Hidden on medium and large screens) -->
+<div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+  <ul class="w3-navbar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
+    <li class="w3-left" style="width:25% !important"><a href="#">HOME</a></li>
+    <li class="w3-left" style="width:25% !important"><a href="#about">ABOUT</a></li>
+    <li class="w3-left" style="width:25% !important"><a href="#photos">PHOTOS</a></li>
+    <li class="w3-left" style="width:25% !important"><a href="#contact">CONTACT</a></li>
+  </ul>
+</div>
+
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- PAGE CONTENT -->
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<div class="w3-padding-large" id="main">
+
+  <!-- ----------------------------------------------------------------------------------------------------------------- -->
+  <!-- HEADER -->
+  <!-- ----------------------------------------------------------------------------------------------------------------- -->
+  <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
+
+    <h1 class="w3-jumbo"><span class="w3-hide-small"></span>Astrotiempo</h1>
+
+    <h3>Significados astronómicos para<br/>nuestra medida cotidiana del tiempo.</h3>
+
+  </header>
+
+  <!-- -----------------------------------------------------------------------------------------------------------------
+  -->
+  <!-- PRESENTACIÓN -->
+  <!-- ----------------------------------------------------------------------------------------------------------------- -->
+  <div class="w3-content w3-justify w3-text-grey" id="inicio">
+    <h2 class="w3-text-light-grey">Presentación</h2>
+    <hr style="width:200px" class="w3-opacity">
+    <p>Some text about me. Some text about me. I am lorem ipsum
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+      in culpa qui officia deserunt mollit anim id est laborum
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </p>
+    
+    <p class="w3-text-gray w3-center">Un proyecto
+      de: <a href="http://astronomia-udea.co" target="_blank">
+	<img src="img/LogoAstronomiaUdeA-Transparente.png" width="250" align="center"></a>
+      <!--Pregrado Astronomía de la Universidad de Antioquia-->
+    </p>
+
+  </div>
+
+  <!-- ----------------------------------------------------------------------------------------------------------------- -->
+  <!-- PERIHELIO -->
+  <!-- ----------------------------------------------------------------------------------------------------------------- -->
+  <!-- About Section -->
+  <div class="w3-content w3-justify w3-text-grey w3-padding-32" id="perihelio">
+    <h2 class="w3-text-light-grey">¿Fin de año?</h2>
+    <hr style="width:200px" class="w3-opacity">
+    <p>Some text about me. Some text about me. I am lorem ipsum
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+      in culpa qui officia deserunt mollit anim id est laborum
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </p>
+
+    <div id="clock" style="width:70%;border:solid white 0px;text-align:center;margin:auto">
+      <div class="clock" style="margin:2em;"></div>
+    </div>
+
+    <p>Some text about me. Some text about me. I am lorem ipsum
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+      in culpa qui officia deserunt mollit anim id est laborum
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </p>
+
+    <script type="text/javascript">
+
+      $(document).ready(function() {
+
+	  // Grab the current date
+	  var currentDate = new Date();
+	  
+	  // Set some date in the future. In this case, it's always Jan 1
+	  var futureDate  = new Date(2017,0,4,9,30,0);
+	  
+	  // Calculate the difference in seconds between the future and current date
+	  var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+	  
+	  // Instantiate a coutdown FlipClock
+	  clock = $('.clock').FlipClock(diff, {
+	      clockFace: 'DailyCounter',
+	      language: 'spanish',
+	      countdown: true
+	  });
+
+      });
+    </script>
+
+  </div>
+
+  <!-- Contact Section -->
+  <div class="w3-padding-64 w3-content w3-text-grey" id="contact">
+    <h2 class="w3-text-light-grey">Contact Me</h2>
+    <hr style="width:200px" class="w3-opacity">
+
+    <div class="w3-section">
+      <p><i class="fa fa-map-marker fa-fw w3-text-white w3-xxlarge w3-margin-right"></i> Chicago, US</p>
+      <p><i class="fa fa-phone fa-fw w3-text-white w3-xxlarge w3-margin-right"></i> Phone: +00 151515</p>
+      <p><i class="fa fa-envelope fa-fw w3-text-white w3-xxlarge w3-margin-right"> </i> Email: mail@mail.com</p>
+    </div><br>
+    <p>Lets get in touch. Send me a message:</p>
+
+    <form action="form.asp" target="_blank">
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Name" required name="Name"></p>
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Email" required name="Email"></p>
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Subject" required name="Subject"></p>
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Message" required name="Message"></p>
+      <p>
+        <button class="w3-btn w3-light-grey w3-padding-large" type="submit">
+          <i class="fa fa-paper-plane"></i> SEND MESSAGE
+        </button>
+      </p>
+    </form>
+  <!-- End Contact Section -->
+  </div>
+  
+  <!-- Footer -->
+  <footer class="w3-content w3-padding-64 w3-text-grey w3-xlarge">
+    <i class="fa fa-facebook-official w3-hover-text-indigo"></i>
+    <i class="fa fa-instagram w3-hover-text-purple"></i>
+    <i class="fa fa-snapchat w3-hover-text-yellow"></i>
+    <i class="fa fa-pinterest-p w3-hover-text-red"></i>
+    <i class="fa fa-twitter w3-hover-text-light-blue"></i>
+    <i class="fa fa-linkedin w3-hover-text-indigo"></i>
+  </footer>
+  <!-- End footer -->
+
+<!-- END PAGE CONTENT -->
+</div>
+
+</body>
+</html>

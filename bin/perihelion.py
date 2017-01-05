f28c68a6ab81afdb186fda48ead28bcd9956a788
Astronomia UdeA <astronomia.udea@gmail.com>
2017-01-05 03:07:10 -0500

    Commit

--- bin/perihelion.py
+++ bin/perihelion.py
@@ -7,7 +7,7 @@ sdate=today.strftime("%m/%d/%y %H:%M:%S")
 
 # Find perihelio for future
 et=spy.str2et(sdate)
-sol=minimize(distanceBodies,(et-1*DAY,et+360*DAY),tol=1e-10,args=(EARTH,SUN))
+sol=minimize(distanceBodies,(et-1*DAY,et+364*DAY),tol=1e-10,args=(EARTH,SUN))
 tper=sol.x
 dt=spy.deltet(tper,"et");
 dper=spy.et2utc(tper,'ISOC',0,100)

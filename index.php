03cf803a04988c8fce70d2915ede29d4d635edd8
Astronomia UdeA <astronomia.udea@gmail.com>
2017-01-22 14:09:14 -0500

    Commit

--- index.php
+++ index.php
@@ -101,7 +101,8 @@ $tlink_terminador=twitterLink($link_terminador,"¿Qué se esta viendo en el term
 
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+  <!--<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">-->
+  <link rel="stylesheet" href="css/w3.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 
   <!-- Font awesome: http://fontawesome.io/icons -->
